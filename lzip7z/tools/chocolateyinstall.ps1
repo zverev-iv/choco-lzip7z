7fb1c3e0191ae415461f4b081a729657eac41c0b
zverev iv <mail@zverev-iv.me>
2021-03-12 17:08:33 +0300

    change csv to pson

--- lzip7z/tools/chocolateyinstall.ps1
+++ lzip7z/tools/chocolateyinstall.ps1
@@ -1,8 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$meta = Get-Content -Path "$($env:ChocolateyPackageFolder)\tools\packageArgs.csv" -Raw
-$packageArgs = @{}
-(ConvertFrom-Csv $meta -Delimiter ';').psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
+$meta = Get-Content -Path $env:ChocolateyPackageFolder\tools\packageArgs.ps1 -Raw | Out-String
+$packageArgs = (Invoke-Expression $meta)
 
 $filename = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
        Split-Path $packageArgs["url64bit"] -Leaf }
---
+++ lzip7z/tools/packageArgs.ps1
@@ -0,0 +1,9 @@
+﻿
+@{
+    url            = "https://www.tc4shell.com/binary/Lzip.zip";
+    checksum       = "839b684384701e4b55a96e351517dc3c97ed727dfc834d96569d02aa2e5baa70";
+    checksumType   = "sha256";
+    url64bit       = "https://www.tc4shell.com/binary/Lzip.zip";
+    checksum64     = "839b684384701e4b55a96e351517dc3c97ed727dfc834d96569d02aa2e5baa70";
+    checksumType64 = "sha256";
+}
